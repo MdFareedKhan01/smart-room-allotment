8ab8d017e206e3803e587889e27724a72acf34a6
Fareed Khan <fareed1234097@gmail.com>
2026-02-27 12:25:27 +0530

    minor testing

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 
-let items=["fareed", "faiqa","Rehan","farhan"];
+let items=["fareed","Rehan","faiqa","farhan"];
 
 
 app.get("/", async (req, res) => {
